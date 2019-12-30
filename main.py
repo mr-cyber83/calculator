f81bbb375efda9fc106ae3b0698d8100ecb79736
Aμιρ <ajahangirzadeh81@gmail.com>
2019-12-30 21:33:32 +0330

    Bugs Fixed

--- main.py
+++ main.py
@@ -16,23 +16,23 @@ option =int( input(">> "))
 if option == 1:
     circle()
 elif option == 2:
-    ethad_2jomlei_negetiv
+    ethad_2jomlei_negetiv()
 elif option == 3:
-    ethad_mozdvj
+    ethad_mozdvj()
 elif option == 4:
-    ethad_2jomlei_plus
+    ethad_2jomlei_plus()
 elif option == 5:
-    mokab_2jomlei_negetiv
+    mokab_2jomlei_negetiv()
 elif option == 6:
-    mokab_2jomlei_plus
+    mokab_2jomlei_plus()
 elif option == 7:
-    fisaqores
+    fisaqores()
 elif option == 8:
-    fat_larg_negetiv
+    fat_larg_negetiv()
 elif option == 9:
-    fat_larg_plus
+    fat_larg_plus()
 elif option == 10:
-    tak_jomlei_mshtrk
+    tak_jomlei_mshtrk()
 elif option == 11:
-    sqrt
+    sqrt()
 else: print("wrong option")
